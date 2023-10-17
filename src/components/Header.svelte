eeae440203b12534248d152ed04a7b1d2324b291
Julio <jcsmerubia@gmail.com>
2023-10-17 15:34:28 +0200

    Intento de nav

---
+++ src/components/Header.svelte
@@ -0,0 +1,37 @@
+<script>
+    import filmin_logo from '../assets/filmin_logo.png'
+</script>
+<main>
+    <header class="site_header c_grid">
+
+        <nav >
+            <a href="">Inicio</a>
+            <a href="">Películas</a>
+            <a href="">Series</a>
+            <a href="">Colecciones</a>
+            <a href="">Festivales</a>
+            <button type="button" class="nav_link">
+                <span>Más</span>
+            </button>
+        </nav>
+    </header>
+</main>
+
+
+<style>
+    header{
+        color: #02FFA1;
+    }
+    .site_header{
+        background-color: #060613;
+        line-height: 1;
+        z-index: 401;
+        position: relative;
+    }
+    nav{
+        align-items: center;
+        flex-wrap: initial;
+    }
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import './app.css'
 import App from './App.svelte'
+import Header from './components/Header.svelte'
 
 const app = new App({
   target: document.getElementById('app'),
 })
 
 export default app
+
+const header = new Header({
+  target: document.getElementById('svelte_container')
+});
+
