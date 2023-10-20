8d0c2c7b7de1c0ff6ccf9ae7c0a57878a354c760
Julio <jcsmerubia@gmail.com>
2023-10-20 10:15:54 +0200

    tabulando cosas

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script>
   import Header from './components/Header.svelte'
-  import MovieList from './components/MovieList.svelte';
   import Slider from './components/Slider.svelte';
 
 </script>
